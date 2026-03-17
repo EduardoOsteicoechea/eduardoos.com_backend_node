c97f5145268fa88f6909c6855d79305658a3c26b
eduar <eduar@EDUARDO>
2026-03-17 02:30:33 -0400

    Generated auth schema for validation middleware (yet to be generated)

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -2,11 +2,28 @@ import { email, z } from 'zod';
 
 export const registerSchema = z.object({
    body: z.object({
-      email: z.email({ message: "invalid email format" })
 
+      email: z.email({ message: "Invalid email format" }),
 
-      password: z.string({}),
+      password: z.string()
+         .min(8,"Password must be at least 8 characters long")
+         .max(64, "Password cannot exceed 64 characters")
+         .regex(/A-Z/, "Password must contain at least one uppercase letter")
+         .regex(/0-9/, "Password must contain at least a number")
+         ,
 
-      name: z.strin
+      name: z.string()
+         .min(2,"Username must be at least 2 characters long")
+         .max(50, "Username cannot exceed 50 characters")
    })
 });
+
+export const loginSchema = z.object({
+   body: z.object({
+      email: z.email({ message: "Invalid email format"}),
+      password: z.string().min(1, "Password cannot be empty")
+   })
+});
+
+export type RegisterInput = z.infer<typeof registerSchema>['body'];
+export type LoginInput = z.infer<typeof loginSchema>['body'];
